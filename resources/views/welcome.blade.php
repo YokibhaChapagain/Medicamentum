f8499664d126095b98fa67ed79144b92c61a2dc9
Yokibha <chapagainyoki25@gmail.com>
2023-10-01 19:35:02 +0545

    Made the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{asset('resources/css/app.css')}}" rel="stylesheet">
-
-    {{-- @vite('resources/css/app.css') --}}
+    @vite('resources/css/app.css')
 </head>
-<body>
-    <h1 class="text-3xl font-bold text-center text-red-400 underline">
-        Hello World!
-    </h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Add your CSS and other head elements here -->
+</head>
+<body >
+
+
+    <!-- Main content container -->
+    <div class="container p-4 mx-auto text-center">
+        <!-- Medicamentum in a larger size -->
+        <div class="fixed left-10 top-60">
+
+        <h1 class="text-5xl font-bold text-teal-700">Medicamentum</h1>
+        <a href="login" class="inline-block px-6 py-2 mt-4 font-semibold text-white bg-teal-500 rounded-lg hover:bg-teal-600">Login</a>
+
+        </div>
+
+        <!-- Landing page image -->
+        <div class="fixed right-10 top-10">
+            <img class="h-192 w-192" src="{{ URL('images/lady.png') }}" alt="vector">
+        </div>
+    </div>
+
+</body>
+</html>
+
+
 </body>
 </html>
