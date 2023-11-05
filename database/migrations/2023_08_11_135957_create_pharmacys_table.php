c3f3f3627126fc4ecd486cdd7a1f5680a09a6048
Yokibha <chapagainyoki25@gmail.com>
2023-11-05 15:18:19 +0545

    Added two new fields in users table

--- database/migrations/2023_08_11_135957_create_pharmacys_table.php
+++ database/migrations/2023_08_11_135957_create_pharmacys_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('telephonenumber')->unique();
             $table->string('location');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
